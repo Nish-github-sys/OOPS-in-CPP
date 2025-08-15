39d6f667e4736745a7f4d2d60f6bd269fbccf726
Nishka Ranadive <nishka.amol.aug2006@gmail.com>
2025-08-15 10:39:20 +0530

    Update and rename exp-11_1.cpp to Exp-11 OOPS in Cpp_1.cpp

--- Exp-11 OOPS in Cpp_1.cpp
+++ Exp-11 OOPS in Cpp_1.cpp
@@ -25,17 +25,19 @@ int main(){
     s1.result=9.27;
 
     Student s2;
-    s2.name="Vishwas";
+    s2.name="Shwetal";
     s2.branch="E&TC";
     s2.subject="CDS";
     s2.year="SY";
-    s2.result=9.01;
+    s2.result=9.26;
 
     cout<<"Name: "<<s1.name<<endl;
     cout<<"Branch: "<<s1.branch<<endl;
     cout<<"Subject: "<<s1.subject<<endl;
     cout<<"Year: "<<s1.year<<endl;
     cout<<"Result: "<<s1.result<<endl;
+
+    cout<<"\n";
 
     cout<<"Name: "<<s2.name<<endl;
     cout<<"Branch: "<<s2.branch<<endl;
@@ -45,3 +47,18 @@ int main(){
 
     return 0;
 }
+
+
+/*==========OUTPUTS==========
+Name: Nishka
+Branch: E&TC
+Subject: CDS
+Year: SY
+Result: 9.27
+
+Name: Shwetal
+Branch: E&TC
+Subject: CDS
+Year: SY
+Result: 9.26
+*/
